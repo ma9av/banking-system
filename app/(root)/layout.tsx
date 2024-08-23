626b50412287f2803feaabd6e88d6dd862f6d0dc
ma9av <manavpanchal42@gmail.com>
2024-08-24 00:00:11 +0530

    "Implement the home page"

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,32 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
     children,
   }: Readonly<{
     children: React.ReactNode;
   }>) {
+      const loggenInUser = {firstName: "John", lastName: "Doe"}
     return (
-        <main>
-            SIDEBAR
+
+        <main className="flex h-screen w-full font-inter">
+            <Sidebar user={loggenInUser}/>
+
+            <div className="flex size-full flex-col">
+                <div className="root-layout">
+                    <Image src="icons/logo.svg" 
+                    width={30}
+                    height={30}
+                    alt="menu icon" />
+                    <div>
+                        <MobileNav user={loggenInUser}/>
+                    </div>
+
+                </div>
             {children}
+
+            </div>
         </main>
     );
   }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,25 @@
+import HeaderBox from "@/components/HeaderBox"
+import RightSideBar from "@/components/RightSideBar"
+import TotalBalanceBox from "@/components/TotalBalanceBox"
+import { log } from "util"
 
 const Home = () => {
+    const loggenInUser = { firstName: 'Manav', lastName: 'Panchal', email: 'manavpanchal42@gmail.com' }
+
   return (
-    <div>
-        Home
-    </div>
+    <section className="home">
+        <div className="home-content">
+            <header className="home-header">
+                <HeaderBox type = "greeting" title = "Welcome" user = {loggenInUser?.firstName || 'Guest'}
+                subtext = "Access and Manage your account and transactions efficiently" />
+
+                <TotalBalanceBox accounts = {[]} totalBanks = {1} totalCurrentBalance = {12500.30}/>
+
+            </header>
+            RECENT TRANSACTIONS
+        </div>
+        <RightSideBar user={loggenInUser} transactions={[]} banks={[{},{}]}/>
+    </section>
   )
 }
 
